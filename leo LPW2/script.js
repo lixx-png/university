16ed656d859dc91982eb0a79ccb1273635219ae9
filipe <luis.filip44@gmail.com>
2025-08-26 21:06:14 -0300

    Update script.js

---
+++ leo LPW2/script.js
@@ -0,0 +1,7 @@
+// pega os elementos do HTML pelo id
+
+const form = document.getElementById('formulario');
+const nome = document.getElementById('nome');
+const email = document.getElementById('email');
+const telefone = document.getElementById('telefone');
+const cpf = document.getElementById('cpf');
